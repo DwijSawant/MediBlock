1aff2662d78905a94b75066cc551227a5809ccbe
interstellar201817 <dwijpramodsawant@gmail.com>
2024-02-11 08:42:06 +0530

    scroll update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from './Homepage/Homepage';
 import Loginpage from './Loginpage/Loginpage';
-import Upload from "./Datapage/Datapage"
+import Upload from "./Datapage/Datapage";
+
 function App() {
   return (
-    <div className='App'>
+    <div className='App'class = 'someName custom-scrollbar' >
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage />} />
